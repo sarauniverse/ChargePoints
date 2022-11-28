546b3e82ed5551ee87e1c5c15aad19311460e03e
Sarangan Gnanasegaram <sarangan.gnanasegaram@cortado.com>
2022-11-28 05:58:17 +0100

    Color changed to match the Cariad theme color

--- app/src/main/java/com/cariad/stations/views/ui/theme/Color.kt
+++ app/src/main/java/com/cariad/stations/views/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.cariad.stations.views.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFF442ee0)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
